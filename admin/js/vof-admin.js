b54eb5416c158444c3fba831bc5febcba0eeb535
Andrej <a.user.de@gmail.com>
2021-01-13 23:01:21 +0100

    change admin input to select

--- admin/js/vof-admin.js
+++ admin/js/vof-admin.js
@@ -1,18 +1,4 @@
 (function( $ ) {
 	'use strict';
-	let mainUrl
-	const adminInput = document.querySelector('#validationCustomUsername')
-	const adminBtn = document.querySelector('#vof-set-admin-btn')
-	adminBtn ? adminBtn.setAttribute('disabled', true) : null
 	
-	const handleSubmit = (e) => {
-		adminBtn.value = ''
-	}
-	const handleChange = (e) => {
-		mainUrl = e.target.value
-		if(mainUrl.length >= 2 && !/[ÄÖÜäöüß]/g.test(mainUrl)) adminBtn.removeAttribute('disabled')
-		else adminBtn.setAttribute('disabled', true)
-	}
-	adminBtn ? adminBtn.addEventListener('click', handleSubmit) : null
-	adminInput ? adminInput.addEventListener('input', handleChange) : null
 })( jQuery );
--- admin/partials/vof-admin-display.php
+++ admin/partials/vof-admin-display.php
@@ -22,19 +22,12 @@ settings_fields( 'vof' );
 do_settings_sections( 'vof' );
 ?>
   <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Bitte tragen Sie Ihre main-url ein: </label>
-    <div class="input-group has-validation">
-      <input name="mainurl" type="text" class="form-control" id="validationCustomUsername" placeholder="" aria-describedby="inputGroupPrepend" required value="<?php echo get_option( 'mainurl' ); ?>">
-      <span class="input-group-text" id="inputGroupPrepend">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
-            <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
-            <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
-        </svg>
-        </span>
-      <div class="invalid-feedback">
-        Bitte überprüfen Sie Ihre Angaben
-      </div>
-    </div>
+    <?php  $selected_option = get_option( 'mainurl' ); ?>
+    <select name="mainurl" class="form-select form-select-lg" aria-label="Default select example">
+        <option selected>Bitte wählen Sie Ihre option: </option>
+        <option value="ch" <?php  if($selected_option === 'ch'){ echo "selected";}; ?>>CH</option>
+        <option value="es" <?php  if($selected_option === 'es'){ echo "selected";}; ?>>ES</option>
+    </select>
   </div>
   <div class="col-12">
     <button class="btn btn-primary" type="submit" id="vof-set-admin-btn">Speichern</button>
